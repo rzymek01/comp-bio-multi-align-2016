092ee0ab454918deea3fcc078d999e24cfb89a4d
Adrian Boguszewski <adrbogus1@student.pg.gda.pl>
2016-01-17 17:30:36 +0100

    Added profile matrix label

--- src/main/java/pg/eti/bio/Controller.java
+++ src/main/java/pg/eti/bio/Controller.java
@@ -2,6 +2,7 @@ package pg.eti.bio;
 
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
+import com.sun.deploy.util.StringUtils;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -28,6 +29,7 @@ import javafx.stage.FileChooser;
 import javafx.util.converter.NumberStringConverter;
 import org.biojava.nbio.alignment.Alignments;
 import org.biojava.nbio.alignment.SimpleGapPenalty;
+import org.biojava.nbio.alignment.SimpleProfile;
 import org.biojava.nbio.alignment.SimpleSubstitutionMatrix;
 import org.biojava.nbio.alignment.SubstitutionMatrixHelper;
 import org.biojava.nbio.alignment.template.AlignedSequence;
@@ -40,7 +42,6 @@ import org.biojava.nbio.core.sequence.ProteinSequence;
 import org.biojava.nbio.core.sequence.compound.AmbiguityDNACompoundSet;
 import org.biojava.nbio.core.sequence.compound.AminoAcidCompound;
 import org.biojava.nbio.core.sequence.compound.AminoAcidCompoundSet;
-import org.biojava.nbio.core.sequence.compound.DNACompoundSet;
 import org.biojava.nbio.core.sequence.compound.NucleotideCompound;
 import org.biojava.nbio.core.sequence.template.CompoundSet;
 import org.slf4j.Logger;
@@ -53,12 +54,15 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static javafx.scene.text.Font.font;
 
+@SuppressWarnings("Duplicates")
 public class Controller implements Initializable {
 
     private static final Logger log = LoggerFactory.getLogger(Controller.class);
@@ -66,6 +70,10 @@ public class Controller implements Initializable {
     private static final ObservableList<String> matrixItems = FXCollections.observableArrayList("nuc4_4", "blosum62", "pam250", "gonnet250", "JOND920103");
     private static final String GAP = "-"; // should be the same as in SimpleAlignedSequence.gap
 
+    @FXML
+    private Label profileMatrix1Label;
+    @FXML
+    private Label profileMatrix2Label;
     @FXML
     private VBox input1VisualVBox;
     @FXML
@@ -195,19 +203,32 @@ public class Controller implements Initializable {
                 .collect(Collectors.toList());
         List<String> outputChains;
         if (type.get().equals(typeItems.get(0))) {
-            outputChains = computeDNAAlignment(chains, gapPenalty, mat);
-        } else {
-            outputChains = computeProteinAlignment(chains, gapPenalty, mat);
-        }
+            Profile<DNASequence, NucleotideCompound> profile = computeDNAAlignment(chains, gapPenalty, mat);
+            List<AlignedSequence<DNASequence, NucleotideCompound>> alignedSequences = profile.getAlignedSequences();
+            outputChains = alignedSequences.stream().map(Object::toString).collect(Collectors.toList());
+            if (compute1Button.equals(event.getSource())) {
+                fillResult(chains, outputChains, input1VisualVBox, output1VisualVBox);
+                fillDNAProfileMatrix(profileMatrix1Label, profile);
+            } else if (compute2Button.equals(event.getSource())) {
+                fillResult(chains, outputChains, input2VisualVBox, output2VisualVBox);
+                fillDNAProfileMatrix(profileMatrix2Label, profile);
 
-        if (compute1Button.equals(event.getSource())) {
-            fillResult(chains, outputChains, input1VisualVBox, output1VisualVBox);
-        } else if (compute2Button.equals(event.getSource())) {
-            fillResult(chains, outputChains, input2VisualVBox, output2VisualVBox);
+            }
+        } else {
+            SimpleProfile<ProteinSequence, AminoAcidCompound> profile = computeProteinAlignment(chains, gapPenalty, mat);
+            List<AlignedSequence<ProteinSequence, AminoAcidCompound>> alignedSequences = profile.getAlignedSequences();
+            outputChains = alignedSequences.stream().map(Object::toString).collect(Collectors.toList());
+            if (compute1Button.equals(event.getSource())) {
+                fillResult(chains, outputChains, input1VisualVBox, output1VisualVBox);
+                fillProteinProfileMatrix(profileMatrix1Label, profile);
+            } else if (compute2Button.equals(event.getSource())) {
+                fillResult(chains, outputChains, input2VisualVBox, output2VisualVBox);
+                fillProteinProfileMatrix(profileMatrix2Label, profile);
+            }
         }
     }
 
-    private List<String> computeDNAAlignment(List<String> chains, GapPenalty gapPenalty, SubstitutionMatrix mat) {
+    private SimpleProfile<DNASequence, NucleotideCompound> computeDNAAlignment(List<String> chains, GapPenalty gapPenalty, SubstitutionMatrix mat) {
         List<DNASequence> proteinSeqs = chains.stream()
                 .map(s -> {
                     try {
@@ -219,13 +240,11 @@ public class Controller implements Initializable {
                 })
                 .collect(Collectors.toList());
 
-        Profile<DNASequence, NucleotideCompound> profile = Alignments.getMultipleSequenceAlignment(proteinSeqs, gapPenalty, mat);
-        final List<AlignedSequence<DNASequence, NucleotideCompound>> alignedSequences = profile.getAlignedSequences();
-
-        return alignedSequences.stream().map(Object::toString).collect(Collectors.toList());
+        SimpleProfile<DNASequence, NucleotideCompound> profile = (SimpleProfile<DNASequence, NucleotideCompound>) Alignments.getMultipleSequenceAlignment(proteinSeqs, gapPenalty, mat);
+        return profile;
     }
 
-    private List<String> computeProteinAlignment(List<String> chains, GapPenalty gapPenalty, SubstitutionMatrix mat) {
+    private SimpleProfile<ProteinSequence, AminoAcidCompound> computeProteinAlignment(List<String> chains, GapPenalty gapPenalty, SubstitutionMatrix mat) {
         List<ProteinSequence> proteinSeqs = chains.stream()
                 .map(s -> {
                     try {
@@ -237,10 +256,48 @@ public class Controller implements Initializable {
                 })
                 .collect(Collectors.toList());
 
-        Profile<ProteinSequence, AminoAcidCompound> profile = Alignments.getMultipleSequenceAlignment(proteinSeqs, gapPenalty, mat);
-        final List<AlignedSequence<ProteinSequence, AminoAcidCompound>> alignedSequences = profile.getAlignedSequences();
+        SimpleProfile<ProteinSequence, AminoAcidCompound> profile = (SimpleProfile<ProteinSequence, AminoAcidCompound>) Alignments.getMultipleSequenceAlignment(proteinSeqs, gapPenalty, mat);
+        return profile;
+    }
 
-        return alignedSequences.stream().map(Object::toString).collect(Collectors.toList());
+    private void fillProteinProfileMatrix(Label matrixLabel, SimpleProfile<ProteinSequence, AminoAcidCompound> profile) {
+        List<String> result = new ArrayList<>(profile.getSize() + 1);
+        result.add("\t");
+        profile.getCompoundSet().getAllCompounds().stream().forEach(c -> result.add(c.getShortName() + "\t"));
+
+        IntStream.range(1, profile.getLength() + 1).forEach(i -> {
+            int[] countsAt = profile.getCompoundCountsAt(i);
+            long count = profile.getCompoundsAt(i).stream().filter(c -> c.equals(profile.getCompoundSet().getCompoundForString(GAP))).count();
+            countsAt[profile.getCompoundSet().getAllCompounds().indexOf(profile.getCompoundSet().getCompoundForString(GAP))] = (int) count;
+            List<Double> doubles = IntStream.range(0, countsAt.length).mapToDouble(idx -> (double) countsAt[idx] / profile.getSize()).boxed().collect(Collectors.toList());
+
+            result.set(0, String.format("%s %-6s", result.get(0), i));
+            for (int j = 0; j < doubles.size(); j++) {
+                result.set(j + 1, String.format("%s %-6s", result.get(j + 1), String.format("%.2f", doubles.get(j))));
+            }
+        });
+        matrixLabel.setText(StringUtils.join(result, "\n"));
+        matrixLabel.setFont(font("Monospace"));
+    }
+
+    private void fillDNAProfileMatrix(Label matrixLabel, Profile<DNASequence, NucleotideCompound> profile) {
+        List<String> result = new ArrayList<>(profile.getSize() + 1);
+        result.add("\t");
+        profile.getCompoundSet().getAllCompounds().stream().forEach(c -> result.add(c.getShortName() + "\t"));
+
+        IntStream.range(1, profile.getLength() + 1).forEach(i -> {
+            int[] countsAt = profile.getCompoundCountsAt(i);
+            long count = profile.getCompoundsAt(i).stream().filter(c -> c.equals(profile.getCompoundSet().getCompoundForString(GAP))).count();
+            countsAt[profile.getCompoundSet().getAllCompounds().indexOf(profile.getCompoundSet().getCompoundForString(GAP))] = (int) count;
+            List<Double> doubles = IntStream.range(0, countsAt.length).mapToDouble(idx -> (double) countsAt[idx] / profile.getSize()).boxed().collect(Collectors.toList());
+
+            result.set(0, String.format("%s %-6s", result.get(0), i));
+            for (int j = 0; j < doubles.size(); j++) {
+                result.set(j + 1, String.format("%s %-6s", result.get(j + 1), String.format("%.2f", doubles.get(j))));
+            }
+        });
+        matrixLabel.setText(StringUtils.join(result, "\n"));
+        matrixLabel.setFont(font("Monospace"));
     }
 
     private void fillBox(String seq, HBox box) {
